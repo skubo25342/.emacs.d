49d1dcf6bfc47d3d855e88486a0a4e2b32dd5396
kubo_shosei@Kubo-MBPR <s.kubo.25342@gmail.com>
2014-07-19 18:11:04 +0900

    Update elpa

---
+++ elpa/dash-20140717.547/dash-autoloads.el
@@ -0,0 +1,18 @@
+;;; dash-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("dash-pkg.el" "dash.el") (21450 13577
+;;;;;;  286625 0))
+
+;;;***
+
+(provide 'dash-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; dash-autoloads.el ends here
---
+++ elpa/helm-20140718.2257/helm-pkg.el
@@ -0,0 +1,1 @@
+(define-package "helm" "20140718.2257" "Helm is an Emacs incremental and narrowing framework" 'nil)
